2bf93fe91b2f1b25dac3e08583c3f5256f91de78
Andrew Yan <zushenyan@gmail.com>
2018-05-01 18:22:03 +0800

    add logger

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 
-express().get('/', (req, res) => res.send('hello world!'))
+const logger = morgan('dev');
+
+express()
+  .use(logger)
+  .get('/', (req, res) => res.send('hello world!'))
   .listen(3000, () => {
     console.log('server starts listening on port 3000...');
   });
